b313f856e8e104423fff5c13a668678621edfb17
mcyc <mcyc@users.noreply.github.com>
2018-03-25 22:24:38 -0700

    updated for gb server run

--- run/run_on_gb.py
+++ run/run_on_gb.py
@@ -20,7 +20,8 @@ class Region(object):
     A Class that contains all the relevant file path and run the pipeline fitting and analysis
     '''
 
-    def __init__(self, region_name, root='base_all_rebase3', rootPara = None):
+    # base_all_rebase3 is no good root for the moment being (e.g., netagive baseline)
+    def __init__(self, region_name, root='all_rebase3', rootPara = None):
 
         self.region = region_name
         self.rootDir = "/lustre/pipeline/scratch/GAS"
@@ -63,6 +64,8 @@ class Region(object):
         self.start_timing()
 
         # defining paths to the imaged data
+        # NOTE: given that there are multiple components, the following attributes are better implamented as
+        # dictoinaries than just a single string
         self.NewParaFile = '{2}/{0}_{3}vcomp_parameter_maps_{1}.fits'.format(self.region, self.rootPara, self.paraDir,
                                                                              n_comp)
         self.ModelFile = '{2}/{0}_NH3_11_{1}_{3}comp_model.fits'.format(self.region, self.root, self.modelDir,
@@ -125,89 +128,29 @@ def make_dir(dirpath):
 #=======================================================================================================================
 
 def super_run():
+    run(region='L1448', root='all_rebase_multi')
+    run(region='OrionB_NGC2023-2024', root='all_rebase_multi')
+    '''
     run(region='OrionA')
     run(region='IC5146')
     run(region='OrionB_NGC2068-2071')
     run(region='Serpens_Aquila')
     run(region='L1688')
+    '''
 
 
-def run_reg():
-    reg = "HC2"
-    run(region=reg)
+def run(region='L1448', multicore=8, root='all_rebase3'):
+    # wrapper to run individual regions on a go
 
-def run(region='L1448', multicore=8):
-
-    regOb = Region(region)
+    regOb = Region(region, root=root)
     regOb.fit_cube(n_comp=1, multicore=multicore, snr_min=5.0, mask_function = None)
     regOb.fit_cube(n_comp=2, multicore=multicore, snr_min=5.0, mask_function = None)
     regOb.calc_aic()
     regOb.calc_chisq()
 
 
-def runOld(region='L1448', multicore=8):
-    # fourier has 8 cores
-    start_time = time.time()
-    print("fitting 1 component")
-    cubefit_gb(region=region, snr_min=5.0, multicore=multicore, n_comp = 1)
-    print("--- %s seconds ---" % (time.time() - start_time))
-
-
-    start_time = time.time()
-    print("fitting 2 component")
-    cubefit_gb(region=region, snr_min=5.0, multicore=multicore, n_comp = 2)
-    print("--- %s seconds ---" % (time.time() - start_time))
-
-
-def cubefit_gb(region='NGC1333', snr_min=5.0, multicore=1, mask_function = None, n_comp = 2):
-    # running the cubefit on green bank server
-
-    # defining directories
-    rootDir = "/lustre/pipeline/scratch/GAS"
-    cubeDir = "{0}/images".format(rootDir)
-    paraDir = "{0}/{1}/paraMaps_MChen".format(cubeDir, region)
-    modelDir = "{0}/{1}/multi_v_models_MChen".format(cubeDir, region)
-    try:
-        os.makedirs(paraDir)
-        os.makedirs(modelDir)
-    except OSError as e:
-        if e.errno != errno.EEXIST:
-            raise
-
-    root = 'base_all_rebase3'
-    rootPara = 'base_all_rebase3'
-
-    # defining paths to imaged data
-    OneOneFile = '{2}/{0}/{0}_NH3_11_{1}.fits'.format(region,root,cubeDir)
-    RMSFile = '{2}/{0}/{0}_NH3_11_{1}_rms.fits'.format(region,root,cubeDir)
-    SingVParaFile = '{2}/{0}/{0}_parameter_maps_{1}.fits'.format(region, rootPara, cubeDir)
-
-    # defining paths to the imaged data
-    NewParaFile = '{2}/{0}_{3}vcomp_parameter_maps_{1}.fits'.format(region, rootPara, paraDir, n_comp)
-    ModelFile = '{2}/{0}_NH3_11_{1}_{3}comp_model.fits'.format(region, root, modelDir, n_comp)
-    ChisqFile = '{2}/{0}_{3}vcomp_chisq_{1}.fits'.format(region, rootPara, paraDir, n_comp)
-
-    SNRFile = '{2}/{0}_{3}vcomp_SNR_eachV_{1}.fits'.format(region, rootPara, paraDir, n_comp)
-    SepVModelFile = '{2}/{0}_NH3_11_{1}_{3}VModel.fits'.format(region, root, modelDir, n_comp)
-
-
-    if os.path.exists(SingVParaFile):
-        guesses = mvf.make_guesses(SingVParaFile, n_comp = n_comp)
-    else:
-        guesses = None
-
-    # fit the data
-    pcubes = mvf.cubefit_gen(cube11name = OneOneFile, ncomp = n_comp , paraname = NewParaFile, modname = ModelFile,
-                             chisqname = ChisqFile, guesses = guesses, errmap11name = RMSFile, multicore = multicore,
-                             snr_min=snr_min, mask_function = mask_function)
-
-    # determine the signal to noise ratio
-    # mchen: a way to determine the average rms based on the RMSFile for individual regions will be better
-    mvf.get_multiV_models(paraname=NewParaFile, refcubename=OneOneFile, n_comp = n_comp, savename = SepVModelFile,
-                                     snrname = SNRFile, rms = 0.15, rmspath=RMSFile)
-
-    return pcubes
-
+'''
 def clean():
     clean_map.clean(oriname, newname, snrname = snrname, one_v_map = one_v_name, aic_maps = aicname,
           fill_plane1 = False, mergesim = False, rm_sml_obj = True, sort_method = "None")
+'''
