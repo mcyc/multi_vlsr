4f8a52beba4748efe5a03fe9c9bc9f8e64e22aaf
mcyc <mcyc@users.noreply.github.com>
2018-04-10 17:05:46 -0700

    changed snr threshold

--- run/run_on_gb.py
+++ run/run_on_gb.py
@@ -174,8 +174,8 @@ def special_run(region='L1448', multicore=8, linename = "oneone"):
 
     regOb.OneOneFile = '{2}/{0}/{0}_NH3_{3}_{1}.fits'.format(regOb.region, 'all_rebase_multi', regOb.cubeDir, regOb.line_root)
 
-    regOb.fit_cube(n_comp=1, multicore=multicore, snr_min=5.0, mask_function = None)
-    #regOb.fit_cube(n_comp=2, multicore=multicore, snr_min=3.0, mask_function = None)
+    #regOb.fit_cube(n_comp=1, multicore=multicore, snr_min=5.0, mask_function = None)
+    regOb.fit_cube(n_comp=2, multicore=multicore, snr_min=5.0, mask_function = None)
     #regOb.calc_aic()
     #regOb.calc_chisq()
 
