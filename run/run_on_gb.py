497448cb1c5264475c34f911661768e0b63ad0ce
mcyc <mcyc@users.noreply.github.com>
2018-04-10 21:55:56 -0700

    changed snr threshold

--- run/run_on_gb.py
+++ run/run_on_gb.py
@@ -148,6 +148,7 @@ def make_dir(dirpath):
 
 
 def super_run():
+    special_run(region='L1448', linename="oneone")
     special_run(region='L1448', linename="twotwo")
     #special_run(region='OrionB_NGC2023-2024')
     '''
@@ -174,8 +175,8 @@ def special_run(region='L1448', multicore=8, linename = "oneone"):
 
     regOb.OneOneFile = '{2}/{0}/{0}_NH3_{3}_{1}.fits'.format(regOb.region, 'all_rebase_multi', regOb.cubeDir, regOb.line_root)
 
-    regOb.fit_cube(n_comp=1, multicore=multicore, snr_min=6.0, mask_function = None)
-    #regOb.fit_cube(n_comp=2, multicore=multicore, snr_min=3.0, mask_function = None)
+    regOb.fit_cube(n_comp=1, multicore=multicore, snr_min=3.0, mask_function = None)
+    regOb.fit_cube(n_comp=2, multicore=multicore, snr_min=3.0, mask_function = None)
     regOb.calc_aic()
     regOb.calc_chisq()
 
