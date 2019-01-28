92e50eaad9cb4e59535076b3e3a26a9303e3af70
Mike Chen <mkid.chen@gmail.com>
2019-01-27 18:39:13 -0800

    enabled fits_comp_AICc to write out the ln-likelihood map. Also, did some code cleanup

--- aic.py
+++ aic.py
@@ -22,8 +22,24 @@ import multi_v_fit as mvf
 
 #=======================================================================================================================
 
+def run():
+    # generate aic maps
 
-def fits_comp_AICc(cubepath, modpath1, modpath2, aiccpath):
+    baseDir = "/Users/mcychen/Documents/Data/GAS_NH3/DR1_rebase3/NGC1333_test"
+
+    cubename = "{0}/NGC1333_NH3_11_DR1_rebase3.fits".format(baseDir)
+    model1name = "{0}/one_v_comp/NGC1333_NH3_11_DR1_rebase3_1comp_model.fits".format(baseDir)
+    model2name = "{0}/two_v_comp/NGC1333_NH3_11_DR1_rebase3_2comp_model.fits".format(baseDir)
+    aiccname = "{0}/two_v_comp/NGC1333_2v1comp_aicc.fits".format(baseDir)
+    lnkname = "{0}/two_v_comp/NGC1333_aicc_lnk_21.fits".format(baseDir)
+
+    fits_comp_AICc(cubename, model1name, model2name, aiccname, lnkname)
+
+
+#=======================================================================================================================
+
+
+def fits_comp_AICc(cubepath, modpath1, modpath2, aiccpath, likelihoodpath = None):
     # a wrapper around fits_comp_AICc() to work with fits files
 
     cube = SpectralCube.read(cubepath)
@@ -36,24 +52,12 @@ def fits_comp_AICc(cubepath, modpath1, modpath2, aiccpath):
     hdr_new['PLANE1'] = "AICc values for the 1 component fit model"
     hdr_new['PLANE2'] = "AICc values for the 2 component fit model"
 
-    aicccube = fits.PrimaryHDU(data=np.array([aicc1, aicc2]), header=cube.wcs.celestial.to_header())
+    aicccube = fits.PrimaryHDU(data=np.array([aicc1, aicc2]), header=hdr_new)
     aicccube.writeto(aiccpath, overwrite=True)
 
-
-
-def run():
-    # generate aic maps
-
-    baseDir = "/Users/mcychen/Documents/Data/GAS_NH3/DR1_rebase3/NGC1333"
-
-    #cubename = "{0}/NGC1333_NH3_11_DR1_rebase3_trim.fits".format(baseDir)
-    cubename = "{0}/NGC1333_NH3_11_DR1_rebase3.fits".format(baseDir)
-    model1name = "{0}/one_v_comp/NGC1333_NH3_11_DR1_rebase3_1comp_model.fits".format(baseDir)
-    model2name = "{0}/two_v_comp/NGC1333_NH3_11_DR1_rebase3_2comp_model.fits".format(baseDir)
-    aiccname = "{0}/two_v_comp/NGC1333_2v1comp_aicc.fits".format(baseDir)
-
-    fits_comp_AICc(cubename, model1name, model2name, aiccname)
-
+    if likelihoodpath is not None:
+        likelyhood = (aicc1 - aicc2) / 2.0
+        fits.writeto(likelihoodpath, likelyhood, cube.wcs.celestial.to_header(), overwrite=True)
 
 
 def fits_comp_chisq(cubepath, modpath1, modpath2, savepath, reduced = True):
@@ -84,7 +88,6 @@ def chi():
     baseDir = "/Users/mcychen/Documents/Data/GAS_NH3/DR1_rebase3/NGC1333"
 
     cubename = "{0}/NGC1333_NH3_11_DR1_rebase3_trim.fits".format(baseDir)
-    #cubename = "{0}/NGC1333_NH3_11_DR1_rebase3.fits".format(baseDir)
     model1name = "{0}/one_v_comp/NGC1333_NH3_11_DR1_rebase3_1comp_model.fits".format(baseDir)
     model2name = "{0}/two_v_comp/NGC1333_NH3_11_DR1_rebase3_2comp_model.fits".format(baseDir)
     chi_name = "{0}/two_v_comp/NGC1333_2v1comp_chisq.fits".format(baseDir)
@@ -114,15 +117,14 @@ def get_comp_AICc(cube, model1, model2, p1, p2):
     mask2 = model2 > 0
     mask = np.logical_or(mask1, mask2)
 
-    #N = np.sum(mask, axis=0)
-
     chi1, N1 = mvf.get_chisq(cube, model1, expand=20, reduced = False, usemask = True, mask = mask)
     chi2, N2 = mvf.get_chisq(cube, model2, expand=20, reduced = False, usemask = True, mask = mask)
 
+    # N = np.sum(mask, axis=0)
     #aicc1 = AICc(chi1, p1, N)
     #aicc2 = AICc(chi2, p2, N)
 
-    # I need a way to double check that N1 and N2 are the same
+    # I need a way to double check that N1 and N2 are the same (just in case)
 
     aicc1 = AICc(chi1, p1, N1)
     aicc2 = AICc(chi2, p2, N1)
