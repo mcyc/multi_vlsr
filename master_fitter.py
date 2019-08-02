48d50cd31478077eaa3fecf31b669884da647d95
Mike Chen <mkid.chen@gmail.com>
2019-08-01 22:27:39 -0700

    added get_best_2comp_residual_cnv() and fit_best_2comp_residual_cnv() methods

--- master_fitter.py
+++ master_fitter.py
@@ -5,8 +5,11 @@ import os
 import numpy as np
 from spectral_cube import SpectralCube
 from astropy import units as u
+from skimage.morphology import dilation
 
 import UltraCube as UCube
+import moment_guess as mmg
+import convolve_tools as cnvtool
 
 #=======================================================================================================================
 
@@ -91,8 +94,69 @@ def iter_2comp_fit(reg, snr_min=3):
         guesses= gss_rf.guess_from_cnvpara(para_cnv, reg.ucube.cube.header, reg.ucube.cube.header)
         reg.ucube.get_model_fit([nc], update=True, guesses=guesses)
 
-def refit_2comp_wide(reg, snr_min=3):
-    best_rms = UCube.get_rms(best_res)
+
+def refit_2comp_wide(reg, window_hwidth=4.0, snr_min=3, res_snr_cut=5):
+    return None
+
+
+
+def get_2comp_wide_guesses(reg, **kwargs):
+    return None
+
+
+
+def fit_best_2comp_residual_cnv(reg, window_hwidth=4.0, **kwargs):
+
+    # need a mechanism to make sure reg.ucube.pcubes['1'], reg.ucube.pcubes['2'] exists
+
+    cube_res_cnv = get_best_2comp_residual_cnv(reg, masked=True, window_hwidth=window_hwidth, **kwargs)
+
+    ncomp = 1
+    moms_res_cnv = mmg.window_moments(cube_res_cnv, window_hwidth=window_hwidth,
+                                      v_atpeak=np.nanmedian(reg.ucube.pcubes['1'].parcube[0]))
+
+    gg = mmg.moment_guesses(moms_res_cnv[1], moms_res_cnv[2], ncomp, moment0=moms_res_cnv[0])
+
+    reg_cnv_rez = UCube.UltraCube(cube=cube_res_cnv)
+    reg_cnv_rez.fit_cube(ncomp=[1], snr_min=3, guesses=gg)
+
+    return reg_cnv_rez
+
+
+def get_best_2comp_residual_cnv(reg, masked=True, window_hwidth=4.0, res_snr_cut=5):
+    # return convolved residual cube with 'excessive' residual above a peak SNR value of res_snr_cut masked
+
+    # need a mechanism to make sure reg.ucube.pcubes['1'], reg.ucube.pcubes['2'] exists
+
+    res_cube = get_best_2comp_residual(reg)
+    best_res = res_cube._data
+    cube_res = SpectralCube(data=best_res, wcs=reg.ucube.pcubes['2'].wcs.copy(),
+                            header=reg.ucube.pcubes['2'].header.copy())
+
+    if masked:
+        #best_rms = UCube.get_rms(best_res)
+        best_rms = UCube.get_rms(res_cube._data)
+
+        # calculate the peak SNR value of the best-fit residual over the main hyperfine components
+        # (note, peak SNR over ALL the hyperfine may pickup residuals due to hyperfine anomolies)
+        res_main_hf = mmg.get_window_slab(res_cube, window_hwidth=window_hwidth,
+                                          v_atpeak=np.nanmedian(reg.ucube.pcubes['1'].parcube[0]))
+
+        res_main_hf_snr = np.nanmax(res_main_hf, axis=0) / best_rms
+
+        # mask out residual with SNR values over the cut threshold
+        mask_res = res_main_hf_snr > res_snr_cut
+        mask_res = dilation(mask_res)
+
+        cube_res_masked = cube_res.with_mask(~mask_res)
+    else:
+        cube_res_masked = cube_res
+
+    cube_res_cnv = cnvtool.convolve_sky_byfactor(cube_res_masked, factor=reg.cnv_factor, edgetrim_width=None,
+                                                 snrmasked=False, iterrefine=False)
+
+    cube_res_cnv = cube_res_cnv.with_spectral_unit(u.km / u.s, velocity_convention='radio')
+    return cube_res_cnv
 
 
 
@@ -117,9 +181,8 @@ def get_best_2comp_residual(reg):
 
     res_cube = SpectralCube(best_res, reg.ucube.cube.wcs)
     res_cube = res_cube.with_spectral_unit(u.km / u.s, velocity_convention='radio')
+
     return res_cube
-
-
 
 
 
