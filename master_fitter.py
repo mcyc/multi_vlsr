16e9031a6471109cf83ee9f30b5fdb0c3968958b
Mike Chen <mkid.chen@gmail.com>
2019-08-02 22:48:25 -0700

    made get_best_2comp_rms_mod() and get_best_2comp_model() into their own modules

--- master_fitter.py
+++ master_fitter.py
@@ -39,6 +39,10 @@ class Region(object):
     def get_fits(self, ncomp, **kwargs):
         get_fits(self, ncomp, **kwargs)
 
+    def load_fits(self, ncomp):
+        # basically the same as get_fits(), but with update set to False to ensure the fits aren't refitted
+        get_fits(self, ncomp, update=False)
+
 
     #def load_model_fit(self, ncomp, update=False):
     #    self.ucube.get_model_fit()
@@ -59,7 +63,7 @@ def get_convolved_cube(reg, update=True, cnv_cubePath=None, edgetrim_width=None,
     reg.cnv_para_paths ={}
 
     if update or (not os.path.isfile(reg.cnv_cubePath)):
-        reg.convolve_cube(factor=reg.cnv_factor, savename=cnv_cubePath, edgetrim_width=edgetrim_width)
+        reg.ucube.convolve_cube(factor=reg.cnv_factor, savename=reg.cnv_cubePath, edgetrim_width=edgetrim_width)
 
     if paraNameRoot is None:
         paraNameRoot = "{}_conv{}Xbeam".format(reg.paraNameRoot, int(np.rint(reg.cnv_factor)))
@@ -72,7 +76,8 @@ def get_convolved_cube(reg, update=True, cnv_cubePath=None, edgetrim_width=None,
 
 def get_convolved_fits(reg, ncomp, **kwargs):
     if not hasattr(reg, 'ucube_cnv'):
-        if 'update' in kwargs:
+        reg.get_convolved_cube(update=True)
+    elif 'update' in kwargs:
             reg.get_convolved_cube(update=kwargs['update'])
     reg.ucube_cnv.get_model_fit(ncomp, **kwargs)
 
@@ -81,27 +86,67 @@ def get_fits(reg, ncomp, **kwargs):
     reg.ucube.get_model_fit(ncomp, **kwargs)
 
 
-def iter_2comp_fit(reg, snr_min=3):
-    # force this to be a two-component fit
+
+
+#=======================================================================================================================
+# functions specific to 2-comonent fits
+
+
+
+def master_2comp_fit(reg, snr_min=3):
+    iter_2comp_fit(reg, snr_min=snr_min)
+    refit_2comp_wide(reg, snr_min=snr_min)
+
+
+def iter_2comp_fit(reg, snr_min=3, updateCnvFits=True):
+    # ensure this is a two compnent fitting method
     ncomp = [1,2]
 
     # convolve the cube and fit it
-    get_convolved_cube(reg, update=False)
-    get_convolved_fits(reg, ncomp, update=False, snr_min=snr_min)
+    get_convolved_cube(reg, update=updateCnvFits)
+    get_convolved_fits(reg, ncomp, update=updateCnvFits, snr_min=snr_min)
 
     # use the result from the convolved cube as guesses for the full resolution fits
     for nc in ncomp:
-        pcube = reg.ucube.pcubes[str(nc)]
-        para_cnv = np.append(pcube.parcube, pcube.errcube, axis=0)
+        pcube_cnv = reg.ucube_cnv.pcubes[str(nc)]
+        para_cnv = np.append(pcube_cnv.parcube, pcube_cnv.errcube, axis=0)
         if nc == 2:
             para_cnv = gss_rf.quick_2comp_sort(para_cnv, filtsize=3)
+
+            # changed the name of the 2-comp para path just for this fit
+            if str(nc) in reg.ucube.paraPaths:
+                oriParaPath_2 = reg.ucube.paraPaths[str(nc)]
+            else:
+                oriParaPath_2 = '{}/{}_{}vcomp.fits'.format(reg.ucube.paraDir, reg.ucube.paraNameRoot, nc)
+
+            noWideParaPath = "{}_noWideDelV.fits".format(os.path.splitext(oriParaPath_2)[0])
+            reg.ucube.paraPaths[str(nc)] = noWideParaPath
+            reg.ucube.paraPaths['2_noWideDelV'] = noWideParaPath
+
         guesses= gss_rf.guess_from_cnvpara(para_cnv, reg.ucube.cube.header, reg.ucube.cube.header)
         reg.ucube.get_model_fit([nc], update=True, guesses=guesses)
 
+    # make sure the two component parapath name returns to normal
+    reg.ucube.paraPaths['2'] = oriParaPath_2
 
 
 
 def refit_2comp_wide(reg, snr_min=3):
+
+    ncomp = [1, 2]
+
+    # load the fitted parameters
+    for nc in ncomp:
+        if not str(nc) in reg.ucube.pcubes:
+            if nc==2 and not ('2_noWideDelV' in reg.ucube.paraPaths):
+                reg.ucube.load_model_fit(reg.ucube.paraPaths[str(nc)], nc)
+                reg.ucube.pcubes['2_noWideDelV'] =\
+                    "{}_noWideDelV".format(os.path.splitext(reg.ucube.paraPaths[str(nc)])[0])
+            else:
+                reg.ucube.load_model_fit(reg.ucube.paraPaths[str(nc)], nc)
+
+    #save_model_fit(self.paraPaths[str(nc)], nc)
+
     wide_comp_guess = get_2comp_wide_guesses(reg)
 
     # use the one component fit and the refined 1-componet guess for the residual to perform the two components fit
@@ -117,21 +162,23 @@ def refit_2comp_wide(reg, snr_min=3):
     lnk10 = reg.ucube.get_AICc_likelihood(1, 0)
     mask = np.logical_and(mask, lnk10 > 5)
 
-    reg_new = UCube.UltraCube(reg.ucube.cubefile)
-    reg_new.fit_cube(ncomp=[2], maskmap=mask, snr_min=snr_min, guesses=final_guess)
+    ucube_new = UCube.UltraCube(reg.ucube.cubefile)
+    ucube_new.fit_cube(ncomp=[2], maskmap=mask, snr_min=snr_min, guesses=final_guess)
 
     # do a model comparison between the new two component fit verses the original one
 
-    lnk_NvsO = UCube.calc_AICc_likelihood(orion_new, 2, 2, ucube_B=reg.ucube)
+    lnk_NvsO = UCube.calc_AICc_likelihood(ucube_new, 2, 2, ucube_B=reg.ucube)
 
     # mask over where one comp fit is more robust
     good_mask = np.logical_and(lnk_NvsO > 0, lnk21 < 5)
 
     reg.ucube_final = UCube.UltraCube(reg.ucube.cubefile)
     pcube_final_2 = reg.ucube.pcubes['2'].copy('deep')
-    replace_para(pcube_final_2, orion_new.pcubes['2'], good_mask)
+    replace_para(pcube_final_2, ucube_new.pcubes['2'], good_mask)
 
-    # a saving function is still needed
+    # save the final fit model
+    UCube.save_model_fit(pcube_final_2, reg.ucube.paraPaths['2'], ncomp=2)
+    #save_model_fit(pcube, savename, ncomp)
 
 
 
@@ -171,7 +218,7 @@ def get_2comp_wide_guesses(reg):
 
 
 
-def fit_best_2comp_residual_cnv(reg, window_hwidth=3.0, res_snr_cut=5):
+def fit_best_2comp_residual_cnv(reg, window_hwidth=3.0, res_snr_cut=5, savefit=True):
 
     # need a mechanism to make sure reg.ucube.pcubes['1'], reg.ucube.pcubes['2'] exists
 
@@ -187,7 +234,12 @@ def fit_best_2comp_residual_cnv(reg, window_hwidth=3.0, res_snr_cut=5):
     reg.ucube_res_cnv = UCube.UltraCube(cube=cube_res_cnv)
     reg.ucube_res_cnv.fit_cube(ncomp=[1], snr_min=3, guesses=gg)
 
-    #return reg_cnv_rez
+    # save the residual fit
+    if savefit:
+        oriParaPath = reg.ucube.paraPaths[str(ncomp)]
+        savename = "{}_onBestResidual.fits".format(os.path.splitext(oriParaPath)[0])
+        reg.ucube_res_cnv.save_model_fit(savename, ncomp)
+
 
 
 def get_best_2comp_residual_cnv(reg, masked=True, window_hwidth=3.0, res_snr_cut=5):
@@ -228,6 +280,22 @@ def get_best_2comp_residual_cnv(reg, masked=True, window_hwidth=3.0, res_snr_cut
 
 
 def get_best_2comp_residual(reg):
+
+    modbest = get_best_2comp_model(reg)
+    best_res = reg.ucube.cube._data - modbest
+    res_cube = SpectralCube(best_res, reg.ucube.cube.wcs)
+    res_cube = res_cube.with_spectral_unit(u.km / u.s, velocity_convention='radio')
+    return res_cube
+
+
+def get_best_2comp_rms_mod(reg):
+    modbest = get_best_2comp_model(reg)
+    res_cube = get_best_2comp_residual(reg)
+    best_rms = UCube.get_rms(res_cube._data)
+    return np.nanmax(modbest, axis=0)/best_rms
+
+
+def get_best_2comp_model(reg):
     # get the log-likelihood between the fits
     #reg.ucube.likelihoods['21'] = reg.ucube.get_AICc_likelihood(2, 1)
     lnk21 = reg.ucube.get_AICc_likelihood(2, 1)
@@ -236,22 +304,16 @@ def get_best_2comp_residual(reg):
     mod2 = reg.ucube.pcubes['2'].get_modelcube()
 
     # get the best model based on the calculated likelihood
-    #mask = reg.likelihoods['21'] > 5
     mask = lnk21 > 5
     modbest = mod1.copy()
     modbest[:, mask] = mod2[:, mask]
 
-    # get the residual of the best fit
+    lnk10 = reg.ucube.get_AICc_likelihood(1, 0)
+    mask = lnk10 < 5
+    modbest[:, mask] = 0
     modbest[np.isnan(modbest)] = 0
-    best_res = reg.ucube.cube._data - modbest
-    #best_rms = UCube.get_rms(best_res)
 
-    res_cube = SpectralCube(best_res, reg.ucube.cube.wcs)
-    res_cube = res_cube.with_spectral_unit(u.km / u.s, velocity_convention='radio')
-
-    return res_cube
-
-
+    return modbest
 
 
 def replace_para(pcube, pcube_ref, mask):
